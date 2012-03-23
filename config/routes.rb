d2260d2f5e7dda98765ab4e3f5d90018b8c6ce04
Justin Baker <azolo1089@gmail.com>
2012-03-23 03:17:27 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ UserNestedPath::Application.routes.draw do
     resources :posts
   end
 
+  root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
